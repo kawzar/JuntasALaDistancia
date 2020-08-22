d5ccca605984b4023969273551aaf12d6ec63bfd
kawzar <hmacarena@gmail.com>
2020-08-22 13:49:58 -0300

    Starting to play with player controls

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerActionType
+{
+    Jump,
+    DestroyObject
+}
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerActionType playerActionType;
+
+    [SerializeField]
+    private bool isActive;
+
+    [SerializeField]
+    private float jumpForce;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private PlayerInput playerInput;
+    private Rigidbody body;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        body = GetComponent<Rigidbody>();
+
+        playerInput.SwitchPlayerKeyPressed += OnSwitchPlayerKeyPressed;
+        playerInput.ActionKeyPressed += OnActionKeyPressed;
+    }
+
+    private void Update()
+    {
+        if (isActive)
+        {
+            Move();
+        }
+    }
+
+    private void OnActionKeyPressed()
+    {
+        if (!isActive) return;
+
+        switch (playerActionType)
+        {
+            case PlayerActionType.Jump:
+                Jump();
+                break;
+            case PlayerActionType.DestroyObject:
+                break;
+        }
+    }
+
+    private void OnSwitchPlayerKeyPressed()
+    {
+        isActive = !isActive;
+    }
+
+    private void Jump()
+    {
+    }
+
+    private void Move()
+    {
+        float positionX = transform.position.x + playerInput.Horizontal * moveSpeed * Time.deltaTime;
+        float positionZ = transform.position.z + playerInput.Vertical * moveSpeed * Time.deltaTime;
+        transform.position = new Vector3(positionX, transform.position.y, positionZ);
+    }
+
+    private void OnDestroy()
+    {
+        playerInput.SwitchPlayerKeyPressed -= OnSwitchPlayerKeyPressed;
+        playerInput.ActionKeyPressed -= OnActionKeyPressed;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float Horizontal { get; private set; }
+    public float Vertical { get; private set; }
+
+    public event Action ActionKeyPressed;
+    public event Action SwitchPlayerKeyPressed;
+
+    [SerializeField]
+    private KeyCode actionKey;
+
+    [SerializeField]
+    private KeyCode switchPlayerKey;
+
+    void Update()
+    {
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(actionKey))
+        {
+            ActionKeyPressed?.Invoke();
+        }
+        else if (Input.GetKeyDown(switchPlayerKey))
+        {
+            SwitchPlayerKeyPressed?.Invoke();
+        }
+    }
+}
