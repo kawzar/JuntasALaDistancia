b2e43c30b809732cf07f6c1abbaa9c3ee2df9624
AnnaSofiaM <anasofia0697@hotmail.com>
2020-08-29 22:50:19 -0500

    UI  Particles

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -7,22 +7,30 @@ public class PlayerInput : MonoBehaviour
 {
     public event Action ActionKeyPressed;
     public event Action SpecialKeyPressed;
+    ParticleSystem mparticle;
 
     [SerializeField]
     private KeyCode actionKey;
 
     [SerializeField]
     private KeyCode specialKey;
+    
+    void Awake()
+    {
+        mparticle = GetComponentInChildren<ParticleSystem>();
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(actionKey))
         {
             ActionKeyPressed?.Invoke();
+            mparticle.Play();
         }
         else if (Input.GetKeyDown(specialKey))
         {
             SpecialKeyPressed?.Invoke();
+            mparticle.Play();
         }
     }
 }
